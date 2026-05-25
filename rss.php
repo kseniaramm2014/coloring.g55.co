7018e0a310fee6b0544eb3ec500f34e366e48749
brvasya <crazygames888@gmail.com>
2026-05-25 19:26:54 +0300

    Create rss.php

---
+++ rss.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'app/index_pre.php';
+$pages = array_slice(array_reverse($pages), 0, 50);
+header('Content-Type: application/rss+xml; charset=utf-8');
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+<channel>
+<title><?php echo h($cat['name']); ?> Coloring Pages</title>
+<link><?php echo h($canonical); ?></link>
+<description><?php echo h(strip_tags($metaDesc)); ?></description>
+<?php foreach ($pages as $p): ?>
+<item>
+<title><?php echo h($p['title']); ?></title>
+<link>https://coloring.g55.co/page.php?id=<?php echo rawurlencode($p['id']); ?>&amp;c=<?php echo rawurlencode($cid); ?></link>
+<guid isPermaLink="true">https://coloring.g55.co/page.php?id=<?php echo rawurlencode($p['id']); ?>&amp;c=<?php echo rawurlencode($cid); ?></guid>
+<description><?php echo h($p['description']); ?></description>
+<media:content url="https://coloring.g55.co/categories/<?php echo rawurlencode($cid); ?>/<?php echo rawurlencode($p['id']); ?>.png" medium="image" type="image/png" />
+</item>
+<?php endforeach; ?>
+</channel>
+</rss>
